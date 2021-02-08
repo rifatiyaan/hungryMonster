1a9bc648d6252bb46d54aeb9da4d5c65ea7fc63b
Bokhtiar Rifat <mdbrifat@gmail.com>
2021-02-08 23:49:04 +0600

    complete

--- app.js
+++ app.js
@@ -1,82 +1,76 @@
-const getFoodDetail = (food) => {
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${food}`;
-    fetch(url)
-        .then((response) => response.json())
-        .then((data) => showFoodGrid(data.meals))
-        .catch((err) => showError());
+const getFoodDetail = (item) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${item}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => showFoodGrid(data.meals))
+    .catch((error) => showError());
 };
 
-const ingredientsData = (food) => {
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${food}`)
-        .then((response) => response.json())
-        .then((data) => sowIngredients(data.meals[0]));
+const ingredientsData = (item) => {
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${item}`)
+    .then((res) => res.json())
+    .then((data) => foodIngredients(data.meals[0]));
 };
 
-const searchFood = () => {
+document
+  .getElementById("searchBtn")
+  .addEventListener("click", function (event) {
     const searchFood = document.getElementById("searchBar").value;
     getFoodDetail(searchFood);
     document.getElementById("searchBar").value = "";
-};
+  });
+
 const showFoodGrid = (data) => {
-    data.forEach((food) => {
-        const searchedFood = `
-        <a onClick="ingredientsData('${food.strMeal}')">       
-        <img src="${food.strMealThumb}">
-        <h3 class="food-name">${food.strMeal}</h3>
+  data.forEach((item) => {
+    const searchedFood = `
+        <a onClick="ingredientsData('${item.strMeal}')">       
+        <img src="${item.strMealThumb}">
+        <h3 class="food-name">${item.strMeal}</h3>
         </a>`;
-        const foodDetails = document.createElement("div");
-        foodDetails.className = "foodResult-card";
-        foodDetails.innerHTML = searchedFood;
-        const showFoodResult = document.getElementById("foodResult-card-grid");
-        showFoodResult.appendChild(foodDetails);
-    });
+    const foodDetails = document.createElement("div");
+    foodDetails.className = "foodResult-card";
+    foodDetails.innerHTML = searchedFood;
+    const showFoodResult = document.getElementById("foodResult-card-grid");
+    showFoodResult.appendChild(foodDetails);
+  });
 };
 
 //Error handling
 const showError = () => {
-    document.getElementById("foodList").style.display = "none";
-    document.getElementById("pop-up").style.display = "block";
+  document.getElementById("foodList").style.display = "none";
+  document.getElementById("pop-up").style.display = "block";
 };
 
-const closeCard = () => {
-    const showIngredients = document.getElementById("ingredients");
-    showIngredients.style.display = "none";
-    const foodContainer = document.getElementById("foodList");
-    foodContainer.style.display = "block";
-};
-
-const sowIngredients = (data) => {
-    const images = data.strMealThumb;
-    const foodName = data.strMeal;
-    const top = `
-    <button onclick="closeCard()" class="close-card">x</button>
-    <img src="${images}" alt="">
+const foodIngredients = (data) => {
+  const images = data.strMealThumb;
+  const foodName = data.strMeal;
+  const text = `
+    
+    <img src="${images}">
         <h3>${foodName}</h3>
         <h4>Ingredients</h4>
-        
     `;
 
-    const ingredientsList = document.createElement("ul");
-    ingredientsList.className = "ingredients-list";
-    const ingredient = "strIngredient";
-    for (let i = 1; i < 11; i++) {
-        const newIngredient = ingredient + i;
-        if (data[newIngredient] == "") {
-            break;
-        } else {
-            const list = document.createElement("li");
-            list.innerHTML = `
+  const ingredientsList = document.createElement("ul");
+  ingredientsList.className = "ingredients-list";
+  const ingredient = "strIngredient";
+  for (let i = 1; i < 10; i++) {
+    const newIngredient = ingredient + i;
+    if (data[newIngredient] == "") {
+      break;
+    } else {
+      const list = document.createElement("li");
+      list.innerHTML = `
             <li style="list-style:square">${data[newIngredient]}</li>
             `;
-            ingredientsList.appendChild(list);
-            console.log(data[newIngredient]);
-        }
+      ingredientsList.appendChild(list);
+      console.log(data[newIngredient]);
     }
-    const showIngredients = document.getElementById("ingredients");
-    showIngredients.innerHTML = top;
-    showIngredients.appendChild(ingredientsList);
-    document.getElementById("foodList").style.display = "none";
-    showIngredients.style.display = "block";
-    console.log(foodName);
-    console.log(data.strIngredient5);
+  }
+
+  const showIngredients = document.getElementById("ingredients");
+  showIngredients.innerHTML = text;
+  showIngredients.appendChild(ingredientsList);
+  document.getElementById("foodList").style.display = "block";
+  showIngredients.style.display = "block";
 };
